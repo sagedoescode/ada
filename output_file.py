70d2cf83d4f8dff8f8ef709f1ade8a7ed02c611f
Sage Dev <111393702+Sageelite123@users.noreply.github.com>
2023-05-16 22:29:21 -0400

    Add files via upload

---
+++ output_file.py
@@ -0,0 +1,62 @@
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(431, 349)
+        MainWindow.setStyleSheet("color: rgb(0, 0, 0);")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        self.my_toggle_button = QtWidgets.QPushButton(self.centralwidget)
+        self.my_toggle_button.setStyleSheet("background-color: rgb(170, 255, 255);")
+        self.my_toggle_button.setObjectName("my_toggle_button")
+        self.verticalLayout_3.addWidget(self.my_toggle_button)
+        self.widget = QtWidgets.QWidget(self.centralwidget)
+        self.widget.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.widget.setObjectName("widget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.widget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.frame = QtWidgets.QFrame(self.widget)
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.frame)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.label = QtWidgets.QLabel(self.frame)
+        font = QtGui.QFont()
+        font.setPointSize(15)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.verticalLayout_2.addWidget(self.label)
+        self.verticalLayout.addWidget(self.frame)
+        self.verticalLayout_3.addWidget(self.widget)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.my_toggle_button.setText(_translate("MainWindow", "Toggle Button"))
+        self.label.setText(_translate("MainWindow", "My Responsive"))
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
